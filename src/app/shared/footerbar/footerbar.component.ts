e8f4bd8df8eafb4692afa380528a1c7954b3f05d
Enrico Hoffmann <dasrick@gmail.com>
2017-09-20 11:08:41 +0200

    add and use dummy component for footerbar

---
+++ src/app/shared/footerbar/footerbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tox-footerbar',
+  templateUrl: './footerbar.component.html',
+})
+export class FooterbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,16 +5,19 @@ import { ApiService } from './services/api.service';
 import { AlertService } from './alert/alert.service';
 import { HeaderbarComponent } from './headerbar/headerbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterbarComponent } from './footerbar/footerbar.component';
 
 @NgModule({
   imports: [],
   declarations: [
     HeaderbarComponent,
     SidebarComponent,
+    FooterbarComponent,
   ],
   exports: [
     HeaderbarComponent,
     SidebarComponent,
+    FooterbarComponent,
   ],
   providers: [
     AlertService,
