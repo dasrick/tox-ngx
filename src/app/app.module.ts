f7db70d3d456acfd1909dfecc45f4d33ee34bbf3
Enrico Hoffmann <dasrick@gmail.com>
2017-09-26 21:08:48 +0200

    restructure 4 re

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// import { FormsModule } from '@angular/forms';
-// import { HttpModule } from '@angular/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-// shared services module
 import { ServicesModule } from './shared/services/services.module';
-// shared components module
 import { SharedComponentsModule } from './shared/components/shared-components.module';
+
 // shared pipes module
 
 @NgModule({
@@ -19,8 +15,6 @@ import { SharedComponentsModule } from './shared/components/shared-components.mo
   ],
   imports: [
     BrowserModule,
-    // FormsModule,
-    // HttpModule,
     AppRoutingModule,
     ServicesModule.forRoot(),
     SharedComponentsModule,
